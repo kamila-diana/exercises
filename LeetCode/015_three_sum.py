a153ad7b542044fa3a86f6d3f0521eba9c014eff
kamiladiana <kamilawinnicka@gmail.com>
2024-06-30 21:33:01 +0200

    leetcode 015 fix

--- LeetCode/015_three_sum.py
+++ LeetCode/015_three_sum.py
@@ -1,19 +1,25 @@
-# 15. 3Sum
+# LeetCode 15. 3Sum
 
 def three_sum(nums: list[int]) -> list[list[int]]:
     result = set()
-    for j, target in enumerate(nums):
-        candidates = {}
-        for i in range(j+1, len(nums)):
-            n = nums[i]
-            if (-target - n) in candidates and i != candidates[-target - n] and j != candidates[-target - n]:
-                result.add(tuple([-target-n, n, target]))
+    sorted_nums = sorted(nums)
+    for target_index, target_value in enumerate(sorted_nums):
+        i = target_index + 1
+        j = len(sorted_nums) - 1
+        while i < j:
+            s = sorted_nums[i] + sorted_nums[j]
+            if s == -target_value:
+                result.add((target_value, sorted_nums[i], sorted_nums[j]))
+                i += 1
+            elif s < - target_value:
+                i += 1
             else:
-                candidates[n] = i
-    deduplicated = set([tuple(sorted(x)) for x in result])
-    return list([list(x) for x in deduplicated])
+                j -= 1
+    return [[x, y, z] for (x, y, z) in result]
 
 
 if __name__ == "__main__":
     a = [1, -2, 1, 0, -1]
+    b = [3, 0, -2, -1, 1, 2]
     assert three_sum(a) == [[-1, 0, 1], [-2, 1, 1]]
+    assert three_sum(b) == [[-2, -1, 3], [-1, 0, 1], [-2, 0, 2]]
